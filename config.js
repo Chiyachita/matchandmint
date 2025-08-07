1a42ef34ad7f4fdf41a7c4eff35a7e877ae34ff4
Chiyachita <118885528+Chiyachita@users.noreply.github.com>
2025-08-07 13:16:02 +0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// config.js
-// 🚨 Paste **exactly** your NFT.Storage key between ASCII single quotes:
-window.NFT_STORAGE_KEY = '6a59a363.70aebce1e531482a9822b21828115ca0';
-console.log('🔑 NFT_STORAGE_KEY length:', window.NFT_STORAGE_KEY.length);
